0ad78016bd2083562281f0ed2b2a01a322587aeb
Twometer <twometer@outlook.de>
2022-08-08 14:19:34 +0200

    Faster index parser

--- src/index.rs
+++ src/index.rs
@@ -3,12 +3,20 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-use rayon::prelude::{ParallelBridge, ParallelIterator};
+use rayon::{
+    prelude::{IntoParallelRefIterator, ParallelBridge, ParallelIterator},
+    slice::ParallelSliceMut,
+};
+
+pub struct Index {
+    entries: Vec<IndexEntry>,
+}
 
 pub struct IndexEntry {
     pub offset: u64,
     pub page_id: u64,
     pub page_name: String,
+    page_name_lowercase: String,
 }
 
 impl IndexEntry {
@@ -18,21 +26,43 @@ impl IndexEntry {
         let offset: u64 = parts.next().unwrap().parse()?;
         let page_id: u64 = parts.next().unwrap().parse()?;
         let page_name = parts.next().unwrap().to_owned();
+        let page_name_lowercase = page_name.to_lowercase();
 
         return Ok(Self {
             offset,
             page_id,
             page_name,
+            page_name_lowercase,
         });
     }
 }
 
-pub fn from_file(file: File) -> Vec<IndexEntry> {
-    let reader = BufReader::new(file);
-    reader
-        .lines()
-        .filter_map(|line| line.ok())
-        .par_bridge()
-        .filter_map(|line| IndexEntry::parse(&line).ok())
-        .collect()
+impl Index {
+    pub fn load(file: &File) -> Index {
+        let reader = BufReader::new(file);
+        let lines: Vec<Result<String, std::io::Error>> = reader.lines().collect();
+        return Index {
+            entries: lines
+                .par_iter()
+                .map(|line| IndexEntry::parse(line.as_ref().unwrap()).unwrap())
+                .collect(),
+        };
+    }
+
+    pub fn find_article(&self, query: &str) -> Vec<&IndexEntry> {
+        let query = query.to_lowercase();
+
+        let mut results = self
+            .entries
+            .iter()
+            .filter(|entry| entry.page_name_lowercase.starts_with(&query))
+            .take(100)
+            .collect::<Vec<&IndexEntry>>();
+        results.sort_by(|a, b| a.page_name.len().cmp(&b.page_name.len()));
+        results
+    }
+
+    pub fn size(&self) -> usize {
+        self.entries.len()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
-use std::fs::File;
+use std::{fs::File, time::Instant};
 
 mod index;
 
 fn main() -> anyhow::Result<()> {
+    let now = Instant::now();
     let index_file =
         File::open(r"X:\Backups\Wikipedia\enwiki-latest-pages-articles-multistream-index.txt")?;
-    let index = index::from_file(index_file);
+    let index = index::Index::load(&index_file);
 
-    println!("Found {} pages in index", index.len());
+    println!(
+        "Found {} pages in index after {:.2?}",
+        index.size(),
+        now.elapsed()
+    );
+
+    let now = Instant::now();
+    let search_results = index.find_article("Rust");
+    println!(
+        "Got {} search results after {:.2?}",
+        search_results.len(),
+        now.elapsed()
+    );
+
+    let article = search_results
+        .first()
+        .expect("No results found for test query");
+
+    println!(
+        "Found article {} at {}/{}",
+        article.page_name, article.offset, article.page_id
+    );
 
     Ok(())
 }
